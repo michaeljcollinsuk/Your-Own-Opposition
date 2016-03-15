5df905c291edf9ccdf66ab74a99124f4b27d0293
ViolaCrellin <viola.crellin@gmail.com>
2016-03-15 18:13:28 +0000

    Calculator now finds political leaning score

--- spec/helpers/url_calculator_spec.rb
+++ spec/helpers/url_calculator_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 describe UrlCalculator, :type => :class do
   let(:daily_mail_url) {'http://www.dailymail.co.uk/home/index.html'}
   let(:telegraph_url) {'http://www.telegraph.co.uk/'}
-  let(:user_urls) {[daily_mail_url, telegraph_url]}
+  let(:guardian_url) {'http://www.theguardian.com/'}
+  let(:user_urls) {[daily_mail_url, telegraph_url, guardian_url]}
   subject(:url_calculator) {described_class.new}
+
   subject(:url_calculator_used) {described_class.new(user_urls)}
-  let(:papers) {{dailymail: :right,
-                 telegraph: :right,
-                 bbc: :center,
-                 guardian: :left,
-                 mirror: :left,
-                 sun: :right,
-                 huffington_post: :center_left,
-                 buzzfeed: :left,
-                 independent: :center_left,
-                 thetimes: :center_right}}
+  let(:papers) {{dailymail: 100,
+                telegraph: 80,
+                bbc: 5,
+                theguardian: -100,
+                mirror: -80,
+                sun: 100,
+                huffington_post: -40,
+                buzzfeed: -20,
+                independent: -20,
+                thetimes: 20}}
 
 
 
@@ -32,6 +34,7 @@ describe UrlCalculator, :type => :class do
       it 'defaults user_urls input to an empty array if no array supplied' do
         expect(url_calculator.user_urls).to be_empty
       end
+
 
       context '#initialized with a url array' do
         subject(:url_calculator_used) {described_class.new(user_urls)}
@@ -60,7 +63,27 @@ describe UrlCalculator, :type => :class do
 
     it 'updates the news_source_list with the names of the matching news sources of urls' do
       url_calculator_used.parse_history
-      expect(url_calculator_used.news_source_list).to include(:telegraph, :dailymail)
+      expect(url_calculator_used.news_source_list).to include(:telegraph, :dailymail, :theguardian)
+    end
+
+  end
+
+  describe '#list_political_leaning_scores' do
+
+    it 'returns an array of the scores out of 100 for political leniencies' do
+      expect(url_calculator_used.political_leaning_scores).to include(80, 100, -100)
+    end
+  end
+
+  describe '#political_leaning_perc' do
+
+    xit 'calls political_leaning_scores' do
+      expect(url_calculator_used).to receive(:political_leaning_scores)
+      url_calculator_used.political_leaning_perc
+    end
+
+    it 'returns a score between -100 and 100 for political leniency' do
+      expect(url_calculator_used.political_leaning_perc).to eq(26)
     end
 
   end
