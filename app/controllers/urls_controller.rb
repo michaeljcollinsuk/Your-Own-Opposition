f46f8efb06c2c6ee9857f48a521b21d807358f95
Michael Collins <michaelj.collinsuk@gmail.com>
2016-03-15 15:27:34 +0000

    User can add link

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,10 +1,16 @@
 class UrlsController < ApplicationController
 
   def index
-    'eggs'
+    @url = Url.all
   end
 
   def create
+    @url = Url.create(url_params)
+    redirect_to '/'
+  end
+
+  def url_params
+    params.require(:url).permit(:name, :link)
   end
 
 end
--- spec/features/urls_feature_spec.rb
+++ spec/features/urls_feature_spec.rb
@@ -11,9 +11,17 @@ feature 'Adding URLs' do
     expect(page).to have_content('Add link')
   end
 
+  scenario 'display users urls' do
+    Url.create(name: 'Guardian', link: 'www.guardian.com')
+    visit '/'
+    expect(page).to have_content('www.guardian.com')
+  end
+
   scenario 'User can add a url' do
-    fill_in 'url', with: 'www.firstlink.com'
-    click_button 'Add'
+    visit '/'
+    fill_in 'Name', with: 'Guardian'
+    fill_in 'Link', with: 'www.firstlink.com'
+    click_button 'Create Url'
     expect(page).to have_content('www.firstlink.com')
   end
 end
