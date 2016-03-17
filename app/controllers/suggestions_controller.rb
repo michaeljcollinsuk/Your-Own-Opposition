4a4db3ef4b95a51b8f65c6682d4e4e8503761737
ViolaCrellin <viola.crellin@gmail.com>
2016-03-17 12:29:21 +0000

    Fixed bugs

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -7,7 +7,7 @@ class SuggestionsController < ApplicationController
     urls = current_user.urls.all
     urls.each { |url| data << url.link }
     suggestion = Suggestion.new(UrlAnalysis, data)
-    suggestion.news_source
+    suggestion.make_suggestion
     render json: suggestion
   end
 end
