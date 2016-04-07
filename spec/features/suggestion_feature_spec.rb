88359d17b0f0594fccd5160790707c7126d26fde
ViolaCrellin <viola.crellin@gmail.com>
2016-04-07 11:34:31 +0100

    Moved backend logic into lib file, refactored spec web_helpers

--- spec/features/suggestion_feature_spec.rb
+++ spec/features/suggestion_feature_spec.rb
@@ -4,7 +4,7 @@ feature 'suggestions' do
 
   before do
     signup
-    create_url2
+    create_url('http://www.theguardian.com/politics/blog/live/2016/mar/17/budget-2016-george-osborne-today-interview-politics-live.com')
   end
 
   scenario 'it responds with a suggestion' do
@@ -12,8 +12,5 @@ feature 'suggestions' do
     expect(page).to have_content 'dailymail'
   end
 
-  # scenario 'it responds with a suggestion' do
-  #   visit '/suggestions'
-  #   expect(page).to have_content 'dailymail'
-  # end
+  #MORE TESTS NEEDED HERE PLEASE!
 end
