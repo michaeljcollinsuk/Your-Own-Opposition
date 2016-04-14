5cba80ababe05f1a39bad0b8089cfde6b7213a3b
Michael Collins <michaelj.collinsuk@gmail.com>
2016-04-14 17:24:16 +0100

    delete unneeded service

--- app/assets/javascripts/services/suggestionService.js
+++
@@ -1,9 +0,0 @@
-UrlsApp.service('SuggestionService', ['$resource', function($resource) {
-
-  //Michael this class should be used for making the API call to webhose
-  // I've not wired up the backend to the AnalysisController - just take a look
-  // at the json the API serves to wire up. Should be self explanatory...
-
-
-
-}]);
