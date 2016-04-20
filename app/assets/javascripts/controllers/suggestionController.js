2e2d42109c90331dd89aaa7c2d31fa2a60340bb7
Michael Collins <michaelj.collinsuk@gmail.com>
2016-04-20 17:04:29 +0100

    refactor suggestion controller

--- app/assets/javascripts/controllers/suggestionController.js
+++ app/assets/javascripts/controllers/suggestionController.js
@@ -2,24 +2,22 @@ UrlsApp.controller('suggestionController', ['suggestionFactory', 'webhoseFactory
   var self = this;
   suggestionFactory.suggestionApiCall().$promise.then(function(response){
     self.suggestions = response;
+    self.papers = Object.keys(self.suggestions.recommended_reading);
   });
 
-  self.showSuggestions = function() {
-    self.suggestionsLoaded = true;
-    self.papers = Object.keys(self.suggestions.recommended_reading);
+  self.getSuggestions = function() {
+    self.showSuggestions = false;
+    self.searchingForLink = true;
+    self.webhoseSearch();
   };
 
-  self.getSuggestions = function() {
-    self.suggestionsLoaded = false;
-    self.searchingForLink = true;
-
+  self.webhoseSearch = function() {
     webhoseFactory.webhoseSuggestions(self.suggestions.best_topic[0], self.papers[0]).$promise.then(function(response) {
-     self.urlLinks = webhoseFactory.getUrlLinks(response.posts);
-     self.articleImages = webhoseFactory.getArticleImages(response.posts);
-     self.articleTitles = webhoseFactory.getArticleTitles(response.posts);
-     self.articleLoaded = true;
-     self.searchingForLink = false;
+      self.urlLinks = webhoseFactory.getUrlLinks(response.posts);
+      self.articleImages = webhoseFactory.getArticleImages(response.posts);
+      self.articleTitles = webhoseFactory.getArticleTitles(response.posts);
+      self.articleLoaded = true;
+      self.searchingForLink = false;
     });
   };
-
 }]);
