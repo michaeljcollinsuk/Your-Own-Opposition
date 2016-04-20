dc08d011bd61301e9027f854896bc4fa7f374b41
Michael Collins <michaelj.collinsuk@gmail.com>
2016-04-20 16:18:13 +0100

    Refactor analysis controller to remove toggle functions

--- app/assets/javascripts/controllers/AnalysisController.js
+++ app/assets/javascripts/controllers/AnalysisController.js
@@ -1,32 +1,10 @@
 UrlsApp.controller('analysisController', ['analysisFactory', function(analysisFactory) {
   var self = this;
-  self.showUserUrls = false;
 
   analysisFactory.analysisApiCall().$promise.then(function(response) {
     self.analysis = response;
-  });
-
-  self.toggleShowRecentUrls = function() {
-    self.showUserUrls = !self.showUserUrls;
-  };
-
-  self.showBias = function() {
-      self.loaded = true;
-      self.bias = analysisFactory.getBias(self.analysis);
-  };
-
-  self.hideBias = function() {
-    self.loaded = false;
-  };
-
-  self.showBreakDown = function(){
-    self.furtherInfoLoaded = true;
     self.sources = analysisFactory.getSources(self.analysis.media_diet);
     self.percentageRead = analysisFactory.getPercentageRead(self.sources, self.analysis.media_diet);
-  };
-
-  self.hideBreakDown = function() {
-    self.furtherInfoLoaded = false;
-  };
-
+    self.bias = analysisFactory.getBias(self.analysis);
+  });
 }]);
