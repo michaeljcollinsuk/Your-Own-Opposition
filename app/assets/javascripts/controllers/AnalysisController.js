adc1082722596f7a8964addf3d1242ade113e042
Michael Collins <michaelj.collinsuk@gmail.com>
2016-04-20 14:40:05 +0100

    refactor controllers

--- app/assets/javascripts/controllers/AnalysisController.js
+++ app/assets/javascripts/controllers/AnalysisController.js
@@ -2,17 +2,17 @@ UrlsApp.controller('analysisController', ['analysisFactory', function(analysisFa
   var self = this;
   self.showUserUrls = false;
 
+  analysisFactory.analysisApiCall().$promise.then(function(response) {
+    self.analysis = response;
+  });
 
   self.toggleShowRecentUrls = function() {
     self.showUserUrls = !self.showUserUrls;
   };
 
   self.showBias = function() {
-    analysisFactory.analysisApiCall().$promise.then(function(response) {
       self.loaded = true;
-      self.analysisResponse = response.bias.political_leaning;
-      self.analysisResponseMessage = response.bias.bias_message;
-    });
+      self.bias = analysisFactory.getBias(self.analysis);
   };
 
   self.hideBias = function() {
@@ -20,15 +20,13 @@ UrlsApp.controller('analysisController', ['analysisFactory', function(analysisFa
   };
 
   self.showBreakDown = function(){
-    analysisFactory.analysisApiCall().$promise.then(function(response) {
-      self.furtherInfoLoaded = true;
-      self.sources = Object.keys(response.media_diet);
-      self.percentageRead = analysisFactory.getPercentageRead(self.sources, response.media_diet);
-  });
+    self.furtherInfoLoaded = true;
+    self.sources = analysisFactory.getSources(self.analysis.media_diet);
+    self.percentageRead = analysisFactory.getPercentageRead(self.sources, self.analysis.media_diet);
+  };
 
   self.hideBreakDown = function() {
     self.furtherInfoLoaded = false;
   };
-};
 
 }]);
--- app/assets/javascripts/controllers/suggestionController.js
+++ app/assets/javascripts/controllers/suggestionController.js
@@ -1,22 +1,19 @@
 UrlsApp.controller('suggestionController', ['suggestionFactory', 'webhoseFactory', function(suggestionFactory, webhoseFactory) {
   var self = this;
+  suggestionFactory.suggestionApiCall().$promise.then(function(response){
+    self.suggestions = response;
+  });
 
   self.showSuggestions = function() {
-    suggestionFactory.suggestionApiCall().$promise.then(function(response){
-      self.suggestionsLoaded = true;
-      self.recommendedReading = response.recommended_reading;
-      self.papers = Object.keys(response.recommended_reading);
-      self.newsSource = self.papers[0];
-      self.topKeyword = response.best_topic[0];
-      self.numberToRead = response.recommended_reading[self.papers[0]];
-    });
+    self.suggestionsLoaded = true;
+    self.papers = Object.keys(self.suggestions.recommended_reading);
   };
 
   self.getSuggestions = function() {
     self.suggestionsLoaded = false;
     self.searchingForLink = true;
 
-    webhoseFactory.webhoseSuggestions(self.topKeyword, self.newsSource).$promise.then(function(response) {
+    webhoseFactory.webhoseSuggestions(self.suggestions.best_topic[0], self.papers[0]).$promise.then(function(response) {
      self.urlLinks = webhoseFactory.getUrlLinks(response.posts);
      self.articleImages = webhoseFactory.getArticleImages(response.posts);
      self.articleTitles = webhoseFactory.getArticleTitles(response.posts);
